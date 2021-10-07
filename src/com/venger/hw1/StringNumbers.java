5ea56c46f9b141698ec9e16943176e643fdca506
Dmytro Venger <dimavengerr@gmail.com>
2021-10-07 14:40:12 +0300

    Initial commit hw1

---
+++ src/com/venger/hw1/StringNumbers.java
@@ -0,0 +1,19 @@
+package com.venger.hw1;
+/*4)Написать метод который вернет String в которой будут в одну строку числа от 1 до 30. Каждое число при этом взять в круглые скобки ()*/
+
+public class StringNumbers {
+    public static void main(String[] args) {
+
+        System.out.println(createString());
+    }
+
+    private static String createString() {
+        StringBuilder strNumbers = new StringBuilder();
+        for (int i = 1; i <= 30; i++) {
+            strNumbers.append("(");
+            strNumbers.append(i);
+            strNumbers.append(")");
+        }
+        return strNumbers.toString();
+    }
+}
