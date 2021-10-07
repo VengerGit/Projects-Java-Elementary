4c71d101341031d41164d198dd084be9b01cef53
Dmytro Venger <dimavengerr@gmail.com>
2021-10-07 15:06:45 +0300

    commit hw1 add task NumbersFrom1to100

---
+++ src/com/venger/hw1/NumbersFrom1to100.java
@@ -0,0 +1,21 @@
+package com.venger.hw1;
+
+/*2) Вывести ряд чисел от 1 до 100 через пробел, с изменениями:
+        - если число кратно 3 то вместо него вывести Hello
+        - если число кратно 5 то вместо него вывести World
+        - если число кратно и 3 и 5 то вместо него вывести HelloWorld*/
+public class NumbersFrom1to100 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("HelloWorld");
+            } else if (i % 3 == 0) {
+                System.out.println("Hello");
+            } else if (i % 5 == 0) {
+                System.out.println("World");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
