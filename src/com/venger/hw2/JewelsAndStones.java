aa0dc497906c8ce32e02689c97b8a49f771e2ab4
Dmytro Venger <dimavengerr@gmail.com>
2021-10-07 14:41:33 +0300

    Initial commit hw2

---
+++ src/com/venger/hw2/JewelsAndStones.java
@@ -0,0 +1,38 @@
+package com.venger.hw2;
+
+/*
+Метод должен вернуть количество драгоценых камней в строке stones.
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+class JewelsAndStones {
+
+    public static void main(String[] args) {
+        String jewels1 = "aA";
+        String stones1 = "aAAbbbb";
+
+        String jewels2 = "z";
+        String stones2 = "ZZ";
+
+        numJewelsInStones(jewels1, stones1);
+        numJewelsInStones(jewels2, stones2);
+
+    }
+
+    public static int numJewelsInStones(String jewel, String stone) {
+        Set<Character> jewels = new HashSet<>();
+        for (int j = 0; j < jewel.length(); j++) {
+            jewels.add(jewel.charAt(j));
+        }
+        int answer = 0;
+        for (int i = 0; i < stone.length(); i++) {
+            if (jewels.contains(stone.charAt(i))) {
+                answer++;
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
---
+++ src/com/venger/hw2/RobotReturnToOrigin.java
@@ -0,0 +1,41 @@
+package com.venger.hw2;
+
+/*
+Задана последовательность движений, вернуть true если оне вернется в исходную точку.
+ */
+
+public class RobotReturnToOrigin {
+
+    public static void main(String[] args) {
+        String moves1 = "UD";
+        String moves2 = "LL";
+        String moves3 = "RRDD";
+
+        judgeCircle(moves1);
+        judgeCircle(moves2);
+        judgeCircle(moves3);
+    }
+
+    public static boolean judgeCircle(String moves) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < moves.length(); i++) {
+            if (moves.charAt(i) == 'U') {
+                y++;
+            } else if (moves.charAt(i) == 'D') {
+                y--;
+            } else if (moves.charAt(i) == 'R') {
+                x++;
+            } else {
+                x--;
+            }
+        }
+        if (x == 0 && y == 0) {
+            System.out.println("true");
+            return true;
+        } else {
+            System.out.println("false");
+            return false;
+        }
+    }
+}
