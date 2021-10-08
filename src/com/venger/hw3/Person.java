e795eef65640a6ec32f430b605723838544b3da9
Dmytro Venger <dimavengerr@gmail.com>
2021-10-08 21:24:32 +0300

    Commit Task1, done

--- src/com/venger/hw3/Person.java
+++ src/com/venger/hw3/Person.java
@@ -7,4 +7,28 @@ package com.venger.hw3;
  */
 
 public class Person {
+    private String surname;
+    private String name;
+    private String patronymic;
+
+    public Person(String surname, String name, String patronymic) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+    }
+
+    public Person(String snp) {
+        String[] parsingSnp = snp.split(" ");
+        this.surname = parsingSnp[0];
+        this.name = parsingSnp[1];
+        this.patronymic = parsingSnp[2];
+    }
+
+
+    public static void main(String[] args) {
+        Person abc = new Person("Иванов Иван Иванович");
+        System.out.printf("%s %s %s\n", abc.name, abc.surname, abc.patronymic);
+        Person abc1 = new Person("Venher", "Dmitriy", "Olegovich");
+        System.out.printf("%s %s %s\n", abc1.name, abc1.surname, abc1.patronymic);
+    }
 }
