d910bc08579813b979c500c57da43cff1cb2557a
Dmytro Venger <dimavengerr@gmail.com>
2021-10-08 11:33:10 +0300

    Initial commit hw3

---
+++ src/com/venger/hw3/PermutationOfTheStringWithTheMethod.java
@@ -0,0 +1,12 @@
+package com.venger.hw3;
+
+/*
+4*) Написать метод у которого 2 входящих параметра типа String.
+Метод должен вернуть true если путем перестановки символов можно из первой строки получить вторую. Например:
+"asdf", "fdsa" => true
+"asdf", "asdfg => false
+"aab", "bba" => false
+ */
+
+public class PermutationOfTheStringWithTheMethod {
+}
---
+++ src/com/venger/hw3/RearrangementOfTheString.java
@@ -0,0 +1,10 @@
+package com.venger.hw3;
+
+/*
+3*) Задана строка. Вывести все перестановки этой строки.
+Например для строки "abc":
+abc, acb, bac, bca, cab, cba
+ */
+
+public class RearrangementOfTheString {
+}
