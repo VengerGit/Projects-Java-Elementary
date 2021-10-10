ab43de3d6f1c2e2acfd68b14303f75702c01fec8
Dmytro Venger <dimavengerr@gmail.com>
2021-10-10 20:08:13 +0300

    Commit Task3,done

--- src/com/venger/hw3/RearrangementOfTheString.java
+++ src/com/venger/hw3/RearrangementOfTheString.java
@@ -7,4 +7,24 @@ abc, acb, bac, bca, cab, cba
  */
 
 public class RearrangementOfTheString {
+
+    public static void main(String[] args) {
+        String rearrangement = "abc";
+        permuteRearrangementOfTheString(rearrangement, "");
+    }
+
+    public static String reverseString(String beginning) {
+        return beginning.substring(1) + beginning.charAt(0);
+    }
+
+    public static void permuteRearrangementOfTheString(String inputString, String answer) {
+        if (inputString.length() == 0) {
+            System.out.println(answer);
+            return;
+        }
+        for (int i = 0; i < inputString.length(); i++) {
+            permuteRearrangementOfTheString(inputString.substring(1), answer + inputString.charAt(0));
+            inputString = reverseString(inputString);
+        }
+    }
 }
