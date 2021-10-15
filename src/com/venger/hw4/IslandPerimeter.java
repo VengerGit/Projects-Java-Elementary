f3c5cb0c72a7317f188673fe2c28ed9a82a47e41
Dmytro Venger <dimavengerr@gmail.com>
2021-10-15 14:31:05 +0300

    Commit Task2(homework4), done

--- src/com/venger/hw4/IslandPerimeter.java
+++ src/com/venger/hw4/IslandPerimeter.java
@@ -10,4 +10,37 @@ package com.venger.hw4;
 
 public class IslandPerimeter {
 
+    public static int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                int num = grid[i][j];
+                if (num == 1) {
+                    if (i == 0 || grid[i - 1][j] == 0) {
+                        perimeter++;
+                    }
+                    if (j == 0 || grid[i][j - 1] == 0) {
+                        perimeter++;
+                    }
+                    if (i == grid.length - 1 || grid[i + 1][j] == 0) {
+                        perimeter++;
+                    }
+                    if (j == grid[i].length - 1 || grid[i][j + 1] == 0) {
+                        perimeter++;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid1 = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        int[][] grid2 = {{1}};
+        int[][] grid3 = {{1, 0}};
+
+        System.out.println(islandPerimeter((grid1)));
+        System.out.println(islandPerimeter(grid2));
+        System.out.println(islandPerimeter(grid3));
+    }
 }
