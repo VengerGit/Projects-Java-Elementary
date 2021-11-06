744fc82fe6e55643b3cec5622e15a5c5872915ef
Dmytro Venger <dimavengerr@gmail.com>
2021-11-06 20:05:07 +0300

    Initial commit HomeWork10, done

---
+++ src/com/venger/hw9/Main.java
@@ -0,0 +1,48 @@
+package com.venger.hw9;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayValueCalculator calculator = new ArrayValueCalculator();
+
+        String[][] input1 = {};
+        String[][] input2 = {
+                {"text", "2", "3", "12"},
+                {"23", "21", "123", "55"},
+                {"11", "01", "34", "11"},
+                {"11", "01", "34", "11"}
+        };
+        String[][] input3 = {
+                {"12", "2", "3", "12"},
+                {"23", "21", "123", "55"},
+                {"11", "01", "34", "A"},
+                {"11", "01", "34", "11"}
+        };
+        String[][] input = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        try {
+            System.out.println(calculator.doCalc(input1));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(calculator.doCalc(input2));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(calculator.doCalc(input3));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+        try {
+            System.out.println(calculator.doCalc(input));
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/com/venger/hw9/MyArraySizeException.java
@@ -0,0 +1,8 @@
+package com.venger.hw9;
+
+public class MyArraySizeException extends Exception {
+
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
