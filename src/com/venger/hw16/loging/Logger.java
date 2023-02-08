ce4b695fc51eacdf533fc68984d5450082dd8de3
VengerGit <91564816+VengerGit@users.noreply.github.com>
2023-02-09 00:16:19 +0200

    Delete src/com/venger/hw16 directory

--- src/com/venger/hw16/loging/Logger.java
+++
@@ -1,6 +0,0 @@
-package com.venger.hw16.loging;
-
-public interface Logger {
-    void debug(String massage);
-    void info(String massage);
-}
--- src/com/venger/hw16/loging/LoggingLevel.java
+++
@@ -1,6 +0,0 @@
-package com.venger.hw16.loging;
-
-public enum LoggingLevel {
-    INFO,
-    DEBUG
-}
